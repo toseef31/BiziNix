83d55ac55ff4136bb963e88818af09743e4148d2
Miloš Kočan <miloskocan@gmail.com>
2022-10-10 00:23:07 +0200

    Add more content on homepage and some content on zivnost page

--- src/views/ZalozenieZivnosti.vue
+++ src/views/ZalozenieZivnosti.vue
@@ -1,20 +1,27 @@
+<template>
+  <div class="bg-no-repeat bg-cover" style="background-image:linear-gradient(0deg, rgba(36,36,39,0.9332107843137255) 40%, rgba(0,0,0,0.29735644257703087) 100%), url('../src/assets/zalozenie-fimy-bg.png')">
+    <div class="max-w-7xl flex h-[80vh] items-center mx-auto py-20 px-4 sm:py-24 sm:px-6 lg:px-8 lg:flex lg:justify-between">
+      <div class="max-w-full">
+        <h2 class="text-4xl font-extrabold text-white sm:text-5xl sm:tracking-tight lg:text-6xl">Jednoduché založenie<br>živnosti za 49 €</h2>
+        <p class="mt-8 mb-8 text-2xl text-white">Zaregistrujte alebo upravte svoju živnost rýchlo, z domu a za najmenšie ceny na trhu. Cena obsahuje aj štátne poplatky v hodnote 49€.</p>
+        <div class="mt-8 text-white">
+          <div class="font-bold text-4xl">Čaka nás</div>
+          <div class="ml-5 mt-2">
+            <ul class="list-disc">
+              <li>Predmet podnikania</li>
+              <li>Osobné údaje</li>
+              <li>Dokončenie objednávky</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="hidden md:block md:max-w-xs lg:max-w-md">
+        <img src="../assets/robot.png">
+      </div>
+    </div>
+  </div>
+</template>
+
 <script setup lang="ts">
 
 </script>
-
-<template>
-    <header class="bg-white shadow">
-      <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-        <h1 class="text-3xl font-bold text-gray-900">Založenie živnosti</h1>
-      </div>
-    </header>
-    <main>
-      <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-        <!-- Replace with your content -->
-        <div class="px-4 py-6 sm:px-0">
-          Content for contact page goes here
-        </div>
-        <!-- /End replace -->
-      </div>
-    </main>
-</template>
