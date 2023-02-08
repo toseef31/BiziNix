c6c4db57dff767842f5c6111903bddf4e97b439b
misoborovsky <misoborovsky@gmail.com>
2023-02-08 11:41:21 +0100

    Added AboutUs texts

--- src/views/Info/AboutUsPage.vue
+++ src/views/Info/AboutUsPage.vue
@@ -6,59 +6,35 @@
     <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
       <!-- Replace with your content -->
       <div class="px-4 py-6 sm:px-0">
-        <p class="font-bold py-6">VITAJTE VO SVETE INOVÁCIÍ!</p>
-        <p>Náš tím právnikov a IT špecialistov si stanovil jediný cieľ:</p>
-
-        <p>
-          Zjednodušiť založenie spoločnosti cez internet tak, ako jednoduché je
-          dnes zriadenie emailového konta.
-        </p>
-
-        <p>
-          Podarilo sa? Portál na zakladanie firiem bizinix.sk je inovatívny
-          nástroj, ktorý využíva najnovšie technológie v oblasti elektronizácie
-          služieb štátnej správy. Prostredníctvom tohto user friendly portálu si
-          dokáže ktokoľvek vytvoriť firmu už za 15 minút. Cez internet, bez
-          potreby vlastniť zložitý zaručený elektronický podpis a bez rád
-          drahých právnikov.
-        </p>
         <p class="py-2">
-          Vďaka inovatívnemu konceptu s plne automatizovanými procesmi môže
-          portál bizinix.sk ponúknuť aj veľmi nízke ceny, pri zaručení vysokého
-          štandardu kvality služieb. Portál spolupracuje s niektorými významnými
-          bankami, advokátmi, daňovými a účtovnými poradcami a odborníkmi v
-          oblasti IT.
+          Sme tím právnikov, ekonómov a IT špecialistov. Naše skúsenosti sme
+          navzájom prepojili a vytvorili sme tento unikátny projekt, ktorého
+          cieľom je pomôcť všetkým podnikateľom.
         </p>
 
-        <p class="pt-10">
-          Portál <b>bizinix.sk</b> je úspešný v týchto mestách:
+        <p class="py-2">
+          Čoraz viac pribúdajúca byrokracia zaťažuje podnikanie natoľko, že
+          snaha podnikať sa presúva čoraz viac k administratíve.
         </p>
-        <table class="w-[300px]">
-          <tr>
-            <td>Bratislava</td>
-            <td class="text-right">Trnava</td>
-          </tr>
-          <tr>
-            <td>Pezinok</td>
-            <td class="text-right">Piešťany</td>
-          </tr>
-          <tr>
-            <td>Senec</td>
-            <td class="text-right">Dunajská Streda</td>
-          </tr>
-          <tr>
-            <td>Malacky</td>
-            <td class="text-right">Galanta</td>
-          </tr>
-        </table>
 
-        <p class="py-6">
-          Ale aj inde na Slovensku:<br />
-          Košice<br />
-          Prešov<br />
-          Žilina<br />
-          Banská Bystrica<br />
-          Nitra
+        <p class="py-2">
+          Preto sme prišli s riešením, ktoré vám ušetrí nielen finančné
+          prostriedky ale predovšetkým čas, ktorého je v podnikaní čoraz menej.
+        </p>
+
+        <p class="py-2">
+          Využívame najmodernejšie nástroje a technológie v oblasti
+          elektronizácie služieb štátnej správy. Za pomoci nášho užívateľom
+          prispôsobenému, si dokážete vytvoriť či upraviť spoločnosť bez potreby
+          drahých právnických kancelárií, pohodlne vystavovať faktúry, či si
+          skontrolovať úradné dokumenty jednoducho i s pomocou smartfónu. Vďaka
+          automatizácií systémov Vám vieme zaručiť vysoký štandard všetkých
+          služieb, komplexnosť a to všetko za bezkonkurenčné ceny.
+        </p>
+
+        <p class="py-2">
+          Aby sme Vám doručili tie najlepšie služby na trhu, spolupracujeme s
+          advokátmi, právnikmi, účtovnými a daňovými poradcami.
         </p>
       </div>
       <!-- /End replace -->
