dff6c886d1ae836f9d97030dc9f15824f73dd15a
misoborovsky <misoborovsky@gmail.com>
2023-07-07 18:10:42 +0200

    Fixed account part of the order documents process

--- src/components/forms/DocumentsOrderComponent.vue
+++ src/components/forms/DocumentsOrderComponent.vue
@@ -161,69 +161,74 @@
             >Prihláste sa</a
           >
         </div>
-        <div class="my-2" v-if="user.userId">
-          <a class="text-teal-500 hover:underline" href="/dashboard"
-            >Prihlásený ako {{ userData.first_name }}
-            {{ userData.last_name }}</a
-          >
-        </div>
         <div>
           <div class="text-2xl font-bold py-4">Účet</div>
-          <div class="grid grid-cols-2 md:grid-cols-3 gap-4 items-center">
-            <FormKit
-              type="text"
-              name="first_name"
-              v-model="userData.first_name"
-              id="first_name"
-              label="Krstné meno"
-              validation="required|length:2"
-            />
-            <FormKit
-              type="text"
-              name="last_name"
-              v-model="userData.last_name"
-              label="Priezvisko"
-              validation="required|length:2"
-            />
+          <div class="my-2" v-if="user.userId">
+            <a class="text-teal-500 hover:underline" href="/dashboard"
+              >Prihlásený ako {{ userData.first_name }}
+              {{ userData.last_name }}</a
+            >
           </div>
-          <div class="grid grid-cols-2 md:grid-cols-3 gap-4">
-            <FormKit
-              type="email"
-              name="email"
-              v-model="userData.email"
-              label="Emailová adresa"
-              :validation-rules="{ emailIsUnique }"
-              validation="required|email|emailIsUnique"
-              :validation-messages="{ emailIsUnique: 'E-mail sa už používa!' }"
-              validation-visibility="live"
-            />
-            <FormKit
-              type="password"
-              autocomplete="new-password"
-              v-model="userData.password"
-              name="password"
-              label="Heslo"
-              validation="required|length:8"
-            />
-            <FormKit
-              type="password"
-              autocomplete="new-password"
-              v-model="userData.password_confirmation"
-              name="password_confirmation"
-              label="Zopakujte heslo"
-              validation="required|confirm:password"
-            />
+          <div v-if="!user.userId">
+            <div class="grid grid-cols-2 md:grid-cols-3 gap-4 items-center">
+              <FormKit
+                type="text"
+                name="first_name"
+                v-model="userData.first_name"
+                id="first_name"
+                label="Krstné meno"
+                validation="required|length:2"
+              />
+              <FormKit
+                type="text"
+                name="last_name"
+                v-model="userData.last_name"
+                label="Priezvisko"
+                validation="required|length:2"
+              />
+            </div>
+            <div class="grid grid-cols-2 md:grid-cols-3 gap-4">
+              <FormKit
+                type="email"
+                name="email"
+                v-model="userData.email"
+                label="Emailová adresa"
+                :validation-rules="{ emailIsUnique }"
+                validation="required|email|emailIsUnique"
+                :validation-messages="{
+                  emailIsUnique: 'E-mail sa už používa!',
+                }"
+                validation-visibility="live"
+              />
+              <FormKit
+                type="password"
+                autocomplete="new-password"
+                v-model="userData.password"
+                name="password"
+                label="Heslo"
+                validation="required|length:8"
+              />
+              <FormKit
+                type="password"
+                autocomplete="new-password"
+                v-model="userData.password_confirmation"
+                name="password_confirmation"
+                label="Zopakujte heslo"
+                validation="required|confirm:password"
+              />
+            </div>
+            <div class="grid grid-cols-2 md:grid-cols-3 gap-4">
+              <FormKit
+                type="text"
+                name="phone"
+                v-model="userData.phone"
+                autocomplete="phone"
+                label="Telefonné číslo (nepovinné)"
+                validation="length:9"
+              />
+            </div>
           </div>
-          <div class="grid grid-cols-2 md:grid-cols-3 gap-4">
-            <FormKit
-              type="text"
-              name="phone"
-              v-model="userData.phone"
-              autocomplete="phone"
-              label="Telefonné číslo (nepovinné)"
-              validation="length:9"
-            />
-          </div>
+
           <FormKit
             type="group"
             v-model="fakturacne_udaje"
@@ -688,15 +693,14 @@ async function isEmailAlreadyRegistered(node: any) {
     const res = await store.dispatch("isEmailAlreadyRegistered", node);
     console.log(res);
     return true;
-  }
-  catch (error) {
+  } catch (error) {
     return false;
   }
 }
 
-async function emailIsUnique(node: any){
-  const result = await isEmailAlreadyRegistered(node.value)
-  return result
+async function emailIsUnique(node: any) {
+  const result = await isEmailAlreadyRegistered(node.value);
+  return result;
 }
 
 async function continueFirstTimeActivation() {
