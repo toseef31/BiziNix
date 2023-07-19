3e4c479b61c6ab6c1255fbf06bf83199350a55b9
Miloš Kočan <miloskoc@youforce.net>
2023-07-19 17:13:35 +0200

    For now add loading only

--- src/views/Common/ThanksYouNewOrder.vue
+++ src/views/Common/ThanksYouNewOrder.vue
@@ -1,22 +1,22 @@
 <template>
     <main class="bg-gray-bizinix text-white">
-      <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-        <div v-if="order && order.order_type === null">
-          <CompanyThanksYou></CompanyThanksYou>
+        <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+          <div v-if="order && order.order_type === null">
+              <CompanyThanksYou></CompanyThanksYou>
+          </div>
+          <div v-else-if="order && order.order_type === 'documents'">
+            <DocumentsThanksYou></DocumentsThanksYou>
+          </div>
+          <div v-else-if="order && order.order_type === 'vhq'">
+            <VhqThanksYouVue></VhqThanksYouVue>
+          </div>
+          <!-- Objednávka nebola prijatá -->
+          <div v-else="!order" class="text-center">
+              <EmojiSadIcon class="text-red-500 w-24 h-24 stroke-1 inline" />
+              <h1 class="text-6xl my-6 font-bold">Ups, niečo sa pokazilo.</h1>
+              <p class="text-xl">Objednávka sa nenašla, alebo nebola prijatá.</p>
+          </div>
         </div>
-        <div v-else-if="order && order.order_type === 'documents'">
-          <DocumentsThanksYou></DocumentsThanksYou>
-        </div>
-        <div v-else-if="order && order.order_type === 'vhq'">
-          <VhqThanksYouVue></VhqThanksYouVue>
-        </div>
-        <!-- Objednávka nebola prijatá -->
-        <div v-else="!order" class="text-center">
-            <EmojiSadIcon class="text-red-500 w-24 h-24 stroke-1 inline" />
-            <h1 class="text-6xl my-6 font-bold">Ups, niečo sa pokazilo.</h1>
-            <p class="text-xl">Objednávka sa nenašla, alebo nebola prijatá.</p>
-        </div>
-      </div>
   </main>
   <FooterLayout></FooterLayout>
 </template>
@@ -34,14 +34,18 @@ import VhqThanksYouVue from "@/components/payments/VhqThanksYou.vue";
 const route = useRoute();
 const order = ref();
 let orderId: any;
+const loading = ref(true)
 
-onBeforeMount(() => {
+const getOrderById = async () => {
+  const orderId = route.params.orderId;
 
-  orderId = route.params.orderId;
-  
-  store.dispatch("getOrderById", orderId).then((response) => {
-    order.value = response.data;
-  });
+  const response = await store.dispatch("getOrderById", orderId);
+  order.value = response.data;
+  loading.value = false;
+};
 
+onBeforeMount( async () => {
+  await getOrderById();
 });
+
 </script>
