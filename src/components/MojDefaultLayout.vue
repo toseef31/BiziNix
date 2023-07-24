5007cfbaac6d7392dbec8aac2d3634078b0dd771
Miloš Kočan <miloskoc@youforce.net>
2023-07-24 23:49:17 +0200

    Add router view and menu with submenu

--- src/components/MojDefaultLayout.vue
+++ src/components/MojDefaultLayout.vue
@@ -1,19 +1,3 @@
-<!--
-  This example requires Tailwind CSS v2.0+ 
-  
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
--->
 <template>
   <!--
     This example requires updating your template:
@@ -46,10 +30,26 @@
               </div>
               <div class="mt-5 h-0 flex-1 overflow-y-auto">
                 <nav class="space-y-1 px-2">
-                  <a v-for="item in navigation" :key="item.name" :href="item.href" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'group flex items-center px-2 py-2 text-base font-medium rounded-md']">
-                    <component :is="item.icon" :class="[item.current ? 'text-gray-300' : 'text-gray-400 group-hover:text-gray-300', 'mr-4 flex-shrink-0 h-6 w-6']" aria-hidden="true" />
-                    {{ item.name }}
-                  </a>
+                  <template v-for="item in navigation" :key="item.name">
+                    <div v-if="!item.children">
+                      <a href="#" :class="[item.current ? 'bg-gray-900 text-white' : 'bg-gray-800 text-gray-300 hover:bg-gray-700 hover:text-white', 'group w-full flex items-center pl-2 py-2 text-sm font-medium rounded-md']">
+                        <component :is="item.icon" :class="[item.current ? 'text-white' : 'text-gray-400 group-hover:text-gray-500', 'mr-3 flex-shrink-0 h-6 w-6']" aria-hidden="true" />
+                        {{ item.name }}
+                      </a>
+                    </div>
+                    <Disclosure as="div" v-else class="space-y-1" v-slot="{ open }">
+                      <DisclosureButton :class="[item.current ? 'bg-gray-900 text-gray-900' : 'bg-gray-800 text-gray-300 hover:bg-gray-900 hover:text-white', 'group w-full flex items-center pl-2 pr-1 py-2 text-left text-sm font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500']">
+                        <component :is="item.icon" class="mr-3 h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-300" aria-hidden="true" />
+                        <span class="flex-1">{{ item.name }}</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" :class="[open ? 'text-gray-400 rotate-90' : 'text-gray-300', 'ml-3 h-5 w-5 flex-shrink-0 transform transition-colors duration-150 ease-in-out group-hover:text-gray-400']" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                          <path fill-rule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clip-rule="evenodd"></path>
+                        </svg>
+                      </DisclosureButton>
+                      <DisclosurePanel class="space-y-1">
+                        <DisclosureButton v-for="subItem in item.children" :key="subItem.name" as="a" :href="subItem.href" class="group flex w-full items-center rounded-md py-2 pl-11 pr-2 text-sm font-medium text-gray-300 hover:bg-gray-900 hover:text-white">{{ subItem.name }}</DisclosureButton>
+                      </DisclosurePanel>
+                    </Disclosure>
+                  </template>
                 </nav>
               </div>
             </DialogPanel>
@@ -69,11 +69,27 @@
           <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
         </div>
         <div class="flex flex-1 flex-col overflow-y-auto">
-          <nav class="flex-1 space-y-1 px-2 py-4">
-            <a v-for="item in navigation" :key="item.name" :href="item.href" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'group flex items-center px-2 py-2 text-sm font-medium rounded-md']">
-              <component :is="item.icon" :class="[item.current ? 'text-gray-300' : 'text-gray-400 group-hover:text-gray-300', 'mr-3 flex-shrink-0 h-6 w-6']" aria-hidden="true" />
-              {{ item.name }}
-            </a>
+          <nav class="flex-1 space-y-1 px-2 py-4 bg-gray-800" aria-label="Sidebar">
+            <template v-for="item in navigation" :key="item.name">
+              <div v-if="!item.children">
+                <a href="#" :class="[item.current ? 'bg-gray-900 text-white' : 'bg-gray-800 text-gray-300 hover:bg-gray-700 hover:text-white', 'group w-full flex items-center pl-2 py-2 text-sm font-medium rounded-md']">
+                  <component :is="item.icon" :class="[item.current ? 'text-white' : 'text-gray-400 group-hover:text-gray-500', 'mr-3 flex-shrink-0 h-6 w-6']" aria-hidden="true" />
+                  {{ item.name }}
+                </a>
+              </div>
+              <Disclosure as="div" v-else class="space-y-1" v-slot="{ open }">
+                <DisclosureButton :class="[item.current ? 'bg-gray-900 text-gray-900' : 'bg-gray-800 text-gray-300 hover:bg-gray-900 hover:text-white', 'group w-full flex items-center pl-2 pr-1 py-2 text-left text-sm font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500']">
+                  <component :is="item.icon" class="mr-3 h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-300" aria-hidden="true" />
+                  <span class="flex-1">{{ item.name }}</span>
+                  <svg xmlns="http://www.w3.org/2000/svg" :class="[open ? 'text-gray-400 rotate-90' : 'text-gray-300', 'ml-3 h-5 w-5 flex-shrink-0 transform transition-colors duration-150 ease-in-out group-hover:text-gray-400']" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                    <path fill-rule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clip-rule="evenodd"></path>
+                  </svg>
+                </DisclosureButton>
+                <DisclosurePanel class="space-y-1">
+                  <DisclosureButton v-for="subItem in item.children" :key="subItem.name" as="a" :href="subItem.href" class="group flex w-full items-center rounded-md py-2 pl-11 pr-2 text-sm font-medium text-gray-300 hover:bg-gray-900 hover:text-white">{{ subItem.name }}</DisclosureButton>
+                </DisclosurePanel>
+              </Disclosure>
+            </template>
           </nav>
         </div>
       </div>
@@ -128,10 +144,11 @@
             <h1 class="text-2xl font-semibold text-gray-900">Dashboard</h1>
           </div>
           <div class="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
+            <RouterView></RouterView>
             <!-- Replace with your content -->
-            <div class="py-4">
+            <!-- <div class="py-4">
               <div class="h-96 rounded-lg border-4 border-dashed border-gray-200" />
-            </div>
+            </div> -->
             <!-- /End replace -->
           </div>
         </div>
@@ -164,15 +181,55 @@ import {
   XIcon,
 } from '@heroicons/vue/outline'
 import { SearchIcon } from '@heroicons/vue/solid'
+import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/vue'
 
 const navigation = [
-  { name: 'Dashboard', href: '#', icon: HomeIcon, current: true },
-  { name: 'Team', href: '#', icon: UsersIcon, current: false },
-  { name: 'Projects', href: '#', icon: FolderIcon, current: false },
-  { name: 'Calendar', href: '#', icon: CalendarIcon, current: false },
-  { name: 'Documents', href: '#', icon: InboxIcon, current: false },
-  { name: 'Reports', href: '#', icon: ChartBarIcon, current: false },
+  { name: 'Dashboard', icon: HomeIcon, current: true, href: '#' },
+  {
+    name: 'Team',
+    icon: UsersIcon,
+    current: false,
+    children: [
+      { name: 'Overview', href: '#' },
+      { name: 'Members', href: '#' },
+      { name: 'Calendar', href: '#' },
+      { name: 'Settings', href: '#' },
+    ],
+  },
+  {
+    name: 'Projects',
+    icon: FolderIcon,
+    current: false,
+    children: [
+      { name: 'Overview', href: '#' },
+      { name: 'Members', href: '#' },
+      { name: 'Calendar', href: '#' },
+      { name: 'Settings', href: '#' },
+    ],
+  },
+  {
+    name: 'Calendar',
+    icon: CalendarIcon,
+    current: false
+  },
+  {
+    name: 'Documents',
+    icon: InboxIcon,
+    current: false,
+    children: [
+      { name: 'Overview', href: '#' },
+      { name: 'Members', href: '#' },
+      { name: 'Calendar', href: '#' },
+      { name: 'Settings', href: '#' },
+    ],
+  },
+  {
+    name: 'Reports',
+    icon: ChartBarIcon,
+    current: false,
+  },
 ]
+
 const userNavigation = [
   { name: 'Your Profile', href: '#' },
   { name: 'Settings', href: '#' },
