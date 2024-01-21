47e2622cc23dd77c005d3eac63635799ddc7b4cc
misoborovsky <misoborovsky@gmail.com>
2024-01-21 17:34:21 +0100

    Removed trial text

--- src/views/User/Register.vue
+++ src/views/User/Register.vue
@@ -3,11 +3,6 @@
     <div class="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
         <div class="sm:mx-auto sm:w-full sm:max-w-md">
             <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Vytvorte si účet</h2>
-            <p class="mt-2 text-center text-sm text-gray-600">
-                Or
-                {{ ' ' }}
-                <a href="#" class="font-medium text-teal-600 hover:text-teal-500"> Štart your 14-day free trial </a>
-            </p>
         </div>
 
         <div class="mt-8 sm:mx-auto sm:w-full">
