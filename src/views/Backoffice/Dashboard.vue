a7ecebff122a43652cf0709f7b59edf4fafb725b
misoborovsky <misoborovsky@gmail.com>
2024-01-30 21:41:37 +0100

    Added loading for Dashboard

--- src/views/Backoffice/Dashboard.vue
+++ src/views/Backoffice/Dashboard.vue
@@ -1,34 +1,38 @@
 <template>
-  
-    <main>
-      <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-        <!-- Replace with your content -->
-        <div class="px-4 py-6 sm:px-0">
-          <h1 class="text-3xl font-bold text-gray-600 pb-10">
-            Moje spoločnosti
-          </h1>
-          <MyCompanies v-if="!isLoading"></MyCompanies>
-          <div class="py-8">
-            <button @click="addCompany" class="w-fit shadow flex justify-center border items-center py-2 px-4 rounded-lg bg-teal-500 border-teal-500 text-white font-bold hover:text-white hover:cursor-pointer hover:bg-gray-500 hover:border-gray-500 space-x-2">
-              <span class="pr-2">Pridať firmu</span>
-            </button>
-          </div>
+  <main>
+    <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+      <!-- Replace with your content -->
+      <div class="px-4 py-6 sm:px-0">
+        <h1 class="text-3xl font-bold text-gray-600 pb-10">
+          Moje spoločnosti
+        </h1>
+        <MyCompanies v-if="!isLoading"></MyCompanies>
+        <div class="py-8">
+          <button @click="addCompany"
+            class="w-fit shadow flex justify-center border items-center py-2 px-4 rounded-lg bg-teal-500 border-teal-500 text-white font-bold hover:text-white hover:cursor-pointer hover:bg-gray-500 hover:border-gray-500 space-x-2">
+            <span class="pr-2">Pridať firmu</span>
+          </button>
         </div>
-        <!-- /End replace -->
       </div>
-    </main>
+      <!-- /End replace -->
+    </div>
+  </main>
 </template>
 
 <script setup lang="ts">
 import MyCompanies from '@/components/MyCompanies.vue';
 import { onMounted, ref } from 'vue';
-import { useStore } from 'vuex'
+import { useStore } from 'vuex';
+import { useRouter } from "vue-router";
 
 const store = useStore();
 const isLoading = ref(true);
+const router = useRouter();
 
 function addCompany() {
-    //todo
+  return router.push({
+    name: "Home",
+  });
 }
 
 onMounted(() => {
