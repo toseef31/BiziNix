1e48fddd64c273a21781f525559ecbc6f86af2bc
misoborovsky <misoborovsky@gmail.com>
2024-02-12 11:36:13 +0100

    Added Sentry

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,7 @@ import { sk } from '@formkit/i18n'
 import myTailwindTheme from '../tailwind-formkit-theme.js';
 import { createVfm } from 'vue-final-modal'
 import 'vue-final-modal/style.css'
+import * as Sentry from "@sentry/vue";
 
 const pro = createProPlugin('fk-d5888c6a7f', inputs)
 //extend defaultConfig for formkit
@@ -46,6 +47,25 @@ const config: DefaultConfigOptions  = {
 
 const app = createApp(App);
 const vfm = createVfm()
+
+Sentry.init({
+  dsn: "https://c8275a8dfc949d1afe49d6853de32557@o4506733218627584.ingest.sentry.io/4506733333512192",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
 app.use(store);
 app.use(createPinia());
 app.use(router);
--- vite.config.mts
+++ vite.config.mts
@@ -3,14 +3,25 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    sourcemap: true, // Source map generation must be turned on
+  },
   server: {
     host: "0.0.0.0",
     port: 8080,
   },
-  plugins: [vue(),vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(), 
+    sentryVitePlugin({
+    authToken: process.env.SENTRY_AUTH_TOKEN,
+    org: "bizinix",
+    project: "fe-vue",
+  }),],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
